3ff9ab6c3ae7abddd62111948502ba51d6f62a18
CharlizeBrodeth <141261729+CharlizeBrodeth@users.noreply.github.com>
2024-03-13 22:21:47 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,341 @@
+//Forkast server side 
+//Install Command:
+//npm init
+//npm i express express-handlebars body-parser mongoose
+
+//Set-up//
+const express = require('express');  
+const server = express(); 
+
+const bodyParser = require('body-parser');
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+const handlebars = require('express-handlebars');
+server.set('view engine', 'hbs');
+server.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+server.use(express.static('public'));
+//End of set-up//
+
+//Connect to DB//
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/Forkastdb');
+
+// import validation helpers
+const {validateEmail, validatePassword, allFieldsProvided} = require('./validationHelpers');
+
+//create collection under Forkastdb//
+const userSchema = new mongoose.Schema({
+    fam_name: { type: String },
+    first_name: { type: String },
+    user_name: { type: String },
+    screen_name: { type: String },
+    bday: {type: Date},
+    email: {type: String},
+    password: {type: String}
+  },{ versionKey: false });
+
+
+//create model for collection//
+const userModel = mongoose.model('user', userSchema);
+
+//create collection for restaurants//
+const restoSchema = new mongoose.Schema({
+    resto_name: {type: String},
+    resto_image: {type:String},
+},{versionKey: false});
+
+//model for resto collection//
+const restoModel = new mongoose.model('restaurant', restoSchema);
+
+
+const resto_reviewSchema = new mongoose.Schema({
+    user_name: {type: String},
+    resto_name: {type: String},
+    review_title: {type: String},
+    review_desc: {type: String},
+    rating: {type: Number}
+},{versionKey: false});
+
+
+const resto_reviewModel = new mongoose.model('resto_reviews', resto_reviewSchema);
+
+
+function errorFn(err){
+    console.log('Error fond. Please trace!');
+    console.error(err);
+}
+
+//Render Home page (start)//
+server.get('/', function(req, resp){
+    //render the home.html
+    resp.render('start', {
+        layout: 'index',
+        title: 'Welcome to Forkast'
+    });
+});
+
+//Render Login page//
+server.get('/login/', function(req, resp){
+    //render the login.html
+    resp.render('login',{
+        layout: 'index',
+        title: 'Login Page'
+    });
+});
+
+global.loggedInUser = '';
+
+//Accepting login//
+server.post('/check_login', function(req, resp){
+    //get inputs//
+    const searchQuery= {email: req.body.email, password: req.body.password};
+    //find in model//
+    userModel.findOne(searchQuery).then(function(user){
+        
+        console.log('checking if registered user');
+        if(user != undefined && user._id != null){
+            //loading home once user logins//
+            //set screen_name to te global variable//
+            global.loggedInUser = user.screen_name;
+            //get all values in restoModel
+            restoModel.find({}).then(function(restaurant){
+                console.log('Retrieving all documents from restoModel');
+                let restoArray = [];
+
+                for(const item of restaurant){
+                    restoArray.push({
+                        _id: item._id.toString(),
+                        resto_name: item.resto_name,
+                        resto_image: item.resto_image
+                    });
+                }
+
+                resp.render('home',{
+                    layout: 'index-home',
+                    title: 'Forkast Home Page',
+                    resto_info: restoArray,
+                    screen_name: global.loggedInUser
+                });
+            }).catch(errorFn);
+        }
+        else{
+            resp.render('result',{
+                layout: 'index',
+                title: 'Result of Action',
+                msg: 'Sorry! wrong Email or Password, please try again',
+                btn_msg: 'Go back to Login',
+                move_to: 'login'
+            });
+        }
+    }).catch(errorFn);
+});
+
+
+//Render Register page//
+server.get('/register/', function(req, resp){
+    //render the register.html
+    resp.render('register',{
+        layout: 'index',
+        title: 'Register Page'
+    });
+});
+
+//Adding a user to the DB//
+server.post('/create_user', async function(req, resp){
+    const requiredFields = ['fam_name', 'first_name', 'user_name', 'screen_name', 'bday', 'email', 'password'];
+    if (!allFieldsProvided(req.body, requiredFields)) {
+        return resp.render('result', {
+            layout: 'index',
+            title: 'Result of Action',
+            msg: 'All fields are required. Please try again.',
+            btn_msg: 'Go back to Registration',
+            move_to: 'register'
+        });
+    }
+
+    if (!validateEmail(req.body.email)) {
+        return resp.render('result', {
+            layout: 'index',
+            title: 'Result of Action',
+            msg: 'Invalid email format. Please try again.',
+            btn_msg: 'Go back to Registration',
+            move_to: 'register'
+        });
+    }
+
+    if (!validatePassword(req.body.password)) {
+        return resp.render('result', {
+            layout: 'index',
+            title: 'Result of Action',
+            msg: 'Password must be at least 8 characters long and include one special character.',
+            btn_msg: 'Go back to Registration',
+            move_to: 'register'
+        });
+    }
+
+    // checks for exisitng username and emal
+    const existingUser = await userModel.findOne({ $or: [{ user_name: req.body.user_name }, { email: req.body.email }] });
+    if (existingUser) {
+        return resp.render('result', {
+            layout: 'index',
+            title: 'Result of Action',
+            msg: 'Username or email already exists. Please try a different one.',
+            btn_msg: 'Go back to Registration',
+            move_to: 'register'
+        });
+    }
+
+    // checks if all fields are validated
+    const userInstance = new userModel(req.body);
+    // save instance to DB
+    userInstance.save().then(function(user){
+        resp.render('result', {
+            layout: 'index',
+            title: 'Result of Action',
+            msg: 'Successfully Registered! Please Proceed to Login',
+            btn_msg: 'Proceed to Login',
+            move_to: 'login'
+        });
+    }).catch(errorFn);
+});
+
+//Render Home page//
+server.get('/home/', function(req, resp){
+    restoModel.find({}).then(function(restaurant){
+        console.log('Retrieving all documents from restoModel');
+        let restoArray = [];
+
+        for(const item of restaurant){
+            restoArray.push({
+                _id: item._id.toString(),
+                resto_name: item.resto_name,
+                resto_image: item.resto_image,
+            });
+        }
+
+        resp.render('home',{
+            layout: 'index-home',
+            title: 'Forkast Home Page',
+            resto_info: restoArray,
+            screen_name: global.loggedInUser
+        });
+    }).catch(errorFn);
+    //render the search-page.html
+});
+
+//Render Search Page//
+server.get('/search/', function(req, resp){
+    restoModel.find({}).then(function(restaurant){
+        let restoArray = [];
+
+        for(const item of restaurant){
+            restoArray.push({
+                resto_name: item.resto_name,
+                resto_image: item.resto_image
+            });
+        }
+
+        resp.render('search',{
+            layout: 'index-search',
+            title: 'Create Review Page',
+            restos: restoArray
+        });
+    }).catch(errorFn);
+    //render the search-page.html
+});
+
+
+//Render View Profile//
+server.get('/profile/:name', function(req, resp){
+    const user = req.params.name;
+    console.log(user);
+
+    const searchUser = {screen_name: user}
+
+    userModel.findOne(searchUser).then(function(user){
+        const searchReview = {user_name: user.user_name}
+
+        resto_reviewModel.find(searchReview).then(function(resto_reviews){
+            console.log('Retrieving all reviews of the user');
+            let reviews = [];
+            for(const item of resto_reviews){
+                reviews.push({
+                    user_name: item.user_name,
+                    resto_name: item.resto_name,
+                    review_title: item.review_title,
+                    review_desc: item.review_desc,
+                    rating: item.rating
+                });
+            }
+
+            resp.render('users',{
+                layout: 'index-users',
+                title: 'Profile' + user.screen_name,
+                profile_name: user.first_name + " " + user.fam_name,
+                user_handle: user.user_name,
+                user_screen: user.screen_name,
+                user_reviews: reviews
+            });
+        }).catch(errorFn);
+    }).catch(errorFn);
+});
+
+//Render See Reviews of Restaurants//from home
+server.get('/review_page/:name/', function(req, resp){
+    const restoName = req.params.name;
+    console.log(restoName);
+    //find all reviews for the given resturant name
+    const searchResto = {resto_name: restoName};
+
+    //get image of restuarant
+    restoModel.findOne(searchResto).then(function(restaurant){
+        const restoImage = restaurant.resto_image;
+
+        //get all reviews of that restaurant
+        resto_reviewModel.find(searchResto).then(function(resto_reviews){
+            console.log('Retrieving all reviews of the restaurant');
+            let reviews = [];
+            for(const item of resto_reviews){
+                reviews.push({
+                    user_name: item.user_name,
+                    resto_name: item.resto_name,
+                    review_title: item.review_title,
+                    review_desc: item.review_desc,
+                    rating: item.rating
+                });
+            }
+            resp.render('reviews',{
+                layout: 'index-reviews',
+                title: 'Review Page'+ restoName,
+                reviews_list: reviews,
+                resto_image: restoImage,
+                restoName : restoName,
+                screen_name: global.loggedInUser
+            });
+        }).catch(errorFn);
+    }).catch(errorFn);
+});
+
+
+//Close DB//
+function finalClose(){
+
+    console.log('Close connection at the end!');
+    mongoose.connection.close();
+    process.exit();
+}
+
+process.on('SIGTERM',finalClose);  //general termination signal
+process.on('SIGINT',finalClose);   //catches when ctrl + c is used
+process.on('SIGQUIT', finalClose); //catches other termination commands
+
+
+//Port//
+const port = process.env.port || 3000;
+server.listen(port, function(){
+    console.log('listening at port' +port);
+});
