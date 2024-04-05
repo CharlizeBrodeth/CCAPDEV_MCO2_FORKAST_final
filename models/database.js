08fcc4d0b9218e2b48a4d503af452053f52322d8
CharlizeBrodeth <141261729+CharlizeBrodeth@users.noreply.github.com>
2024-04-05 22:15:01 +0800

    Add files via upload

--- models/database.js
+++ models/database.js
@@ -1,6 +1,10 @@
 //Connect to DB//
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Forkastdb');
+//mongoose.connect('mongodb://127.0.0.1:27017/Forkastdb');
+
+mongoose.connect('mongodb://127.0.0.1:27017/Forkastdb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
 
 
 // create collection for available avatars 
