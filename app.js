83482014763710fa976c7c91beffdd3cb9b98371
CharlizeBrodeth <141261729+CharlizeBrodeth@users.noreply.github.com>
2024-04-05 21:00:18 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,20 +3,76 @@
 //npm init
 //npm i express express-handlebars body-parser mongoose bcrypt express-session
 
+
+const database = require('../models/database');
+
+const userModel = database.userModel;
+const restoModel = database.restoModel;
+const resto_reviewModel = database.resto_reviewModel;
+const avatarModel = database.avatarModel;
+
+const fs = require('fs/promises');
+async function importData() {
+    try {
+        // Read data from JSON files
+        const userData = await fs.readFile('./Forkastdb.users.json', 'utf8');
+        const restoData = await fs.readFile('./Forkastdb.restaurants.json', 'utf8');
+        const resto_reviewData = await fs.readFile('./Forkastdb.resto_reviews.json', 'utf8');
+        const avatarData = await fs.readFile('./Forkastdb.avatars.json', 'utf8');
+        
+        // Parse JSON data
+        const users = JSON.parse(userData);
+        const restaurants = JSON.parse(restoData);
+        const reviews = JSON.parse(resto_reviewData);
+        const avatars = JSON.parse(avatarData);
+    
+
+        // Check if data already exists in collections
+        const existingUsers = await userModel.countDocuments();
+        const existingRestaurants = await restoModel.countDocuments();
+        const existingReviews = await resto_reviewModel.countDocuments();
+        const existingAvatars = await avatarModel.countDocuments();
+       
+
+        // Insert data into MongoDB collections if they don't exist
+        if (existingUsers === 0) {
+            await userModel.insertMany(users);
+            console.log('User accounts imported');
+        }
+
+        if (existingRestaurants === 0) {
+            await restoModel.insertMany(restaurants);
+            console.log('Restaurants imported');
+        }
+
+        if (existingReviews === 0) {
+            await resto_reviewModel.insertMany(reviews);
+            console.log('Reviews imported');
+        }
+
+        if (existingAvatars === 0) {
+            await avatarModel.insertMany(avatars);
+            console.log('Avatars imported');
+        }
+
+
+        console.log('Data import completed');
+    } catch (error) {
+        console.error('Error importing data:', error);
+    }
+}
+importData();
+
+
 //Set-up//
 const express = require('express');  
 const server = express(); 
 const session = require('express-session');
-
-server.use(session({
-    secret:'secret-keyyy',
-    resave:false,
-    saveUninitialized:false,
-    cookie: { secure: false }
-}));
-
+// Password Hashing
+const bcrypt = require('bcrypt');
 
 const bodyParser = require('body-parser');
+server.use(bodyParser.json());
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
@@ -27,7 +83,7 @@ server.engine('hbs', handlebars.engine({
 }));
 
 
-const controllers = ['routes']; //ung mga get eme nasa controller
+const controllers = ['routes']; 
 for(var i=0; i<controllers.length; i++){
   const model = require('./controllers/'+controllers[i]);
   model.add(server);
@@ -35,6 +91,9 @@ for(var i=0; i<controllers.length; i++){
 
 server.use(express.static('public'));
 //End of set-up//
+
+
+
 
 
 //Port//
